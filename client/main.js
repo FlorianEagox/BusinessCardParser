a9ca0eb5c78e51ee47374efdf97c59bc4d85618f
seth <seth@sethpainter.com>
2020-12-01 17:55:50 +0000

    Finished an EPIC frontend

--- client/main.js
+++ client/main.js
@@ -1,30 +1,63 @@
-const btnParse = document.querySelector('#btnParse');
-const txtText = document.querySelector('#txtText');
-const fileImage = document.querySelector('#fileImage');
 const serverURL = 'http://localhost:3000';
-const imageMode = true;
+const rdbMode = document.querySelector('#rdb-text');
+const txtText = document.querySelector('#txt-text');
+const fileImage = document.querySelector('#file-image');
+const btnParse = document.querySelector('#btn-parse');
+const elErrorBox = document.querySelector('#error-box');
+const elError =	document.querySelector('#error');
+const loader = document.querySelector('#loader');
 
 btnParse.addEventListener('click', async () => {
-	let options;
-	if(!imageMode)
-		options = {
-			headers: {
-				"content-type": "application/json"
-			},
-			body: JSON.stringify({text: txtText.value}),
+	let options; // options for the request
+	const textMode = rdbMode.checked; // tells us which tab is selected
+	if(textMode) { // We're uploading card text
+		if(txtText.value)
+			options = {
+				headers: { "content-type": "application/json" },
+				body: JSON.stringify({text: txtText.value})
+			};
+		else {
+			displayError('You must enter the text to send');
+			return;
 		}
-	else {
+	} else { // we're uploading an image
+		if(!fileImage.files[0]) {
+			displayError('You must upload an image!');
+			return;
+		}
 		const form = new FormData();
 		form.append("image", fileImage.files[0]);
-		options = {
-			body: form
-		}
+		options = { body: form };
 	}
 	try {
-		const cardData = await fetch(`${serverURL}/${imageMode ? 'image' : 'text'}`, {method: 'POST', ...options})
+		loader.classList.add('loader');
+		const cardData = await fetch(`${serverURL}/${textMode ? 'text' : 'image'}`, {method: 'POST', ...options})
 		if(cardData.ok)
-			console.log(await cardData.json());
+			displayCard(await cardData.json());
+		else
+			displayError(cardData);
 	} catch(error) {
 		console.log(error)
 	}
 });
+
+function displayCard(cardData) {
+	console.log(cardData);
+	const lblEmail = document.querySelector('#lbl-email');
+	const lblPhone = document.querySelector('#lbl-phone');
+	const lblName = document.querySelector('#lbl-name');
+	const {name, phone, email} = cardData
+	lblName.textContent = name;
+	lblPhone.textContent = phone;
+	lblPhone.href = `tel:${phone}`;
+	lblEmail.textContent = email;
+	lblEmail.href = `mailto:${email}`;
+	loader.classList.remove('loader');
+}
+function displayError(error) {
+	console.error(error);
+	elError.textContent = error;
+	elErrorBox.classList.add('active');
+	setTimeout(() => elErrorBox.classList.remove('active'), 3000);
+	loader.classList.remove('loader');
+}
